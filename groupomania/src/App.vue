9ca61b4f10d8299f81f4668ee60890a07b53edbd
Fred DUCHER <fred-devproject@gmail.com>
2020-09-12 09:39:36 +0200

    FRONTEND / component header

---
+++ groupomania/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+  <header-top></header-top>  
+  </div>
+</template>
+
+<script>
+import header_top from './components/header/Header-top.vue'
+
+export default {
+  name: 'App',
+  components: {
+    'header-top': header_top,
+  }
+}
+</script>
+
+<style>
+
+</style>
